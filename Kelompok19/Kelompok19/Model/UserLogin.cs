14bbeed5cf970575001c1edf4c6accb4aff4a3f5
Muhammad Ikhsan <Muhammadikhsan1042@gmail.com>
2018-10-23 13:31:42 +0700

    Modul1

---
+++ Kelompok19/Kelompok19/Model/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kelompok19.Model
+{
+    class UserLogin
+    {
+        public static string Username = "Kelompok19";
+        public static string Password = "password";
+    }
+}
